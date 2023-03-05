0ab70d92a3861c3621e09f4ce7ead4ac777ed6dd
LordKajoc <rizkinuriman@upi.edu>
2023-03-05 23:21:02 +0700

    Penambahan Button dan Toast

--- app/src/main/java/com/example/dummytestgitlab/MainActivity.kt
+++ app/src/main/java/com/example/dummytestgitlab/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.dummytestgitlab
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            Toast.makeText(this, "Berhasil",Toast.LENGTH_LONG).show()
+        }
     }
 }
